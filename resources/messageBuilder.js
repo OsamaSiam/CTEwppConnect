3659fda74838e6ddad2b01a9f26088c6041d21ac
OsamaSiam <o19siam91a@gmail.com>
2022-05-24 01:34:47 +0300

    Second Commit to add the other files

---
+++ resources/messageBuilder.js
@@ -0,0 +1,159 @@
+let diction = {
+  MATH301: 'Mathematics-1',
+  PHY301: 'ppphysics',
+  ENGL301: 'Enlish Language-1',
+  INSA312: 'Basic Networks System Administration',
+  INSA351: 'Networ Technologies 1',
+  INSA343: 'Problem Sloving Strategies',
+  MATH303: 'Discrete Math',
+  GNRL401: 'Intro to Management and Leadership',
+  INSA371: 'Advanced Network Administration',
+  INSA452: 'Network Technologies 2',
+  STAT303: 'Statistics and Probability',
+  ENGL302: 'English Langauge 2',
+  INSA453: 'Data Center Opertion 1',
+  INSA482: 'Ethics in IT',
+  INET433: 'Inofrmation and Network Security',
+  GNRL405: 'Engineering Economy',
+  INSA443: 'Network Analysis and Design',
+  INSA454: 'Data Center Opertion 2',
+  INSA483: 'Seminar',
+  GNRL402: 'Engieering Projects Management',
+  INET434: 'Cyber Security',
+  INSA484: 'IT infrastructure Best Practices',
+  INSA492: 'Graduation Project',
+  IPRG335: 'Advanced Web Programming',
+  INSA481: 'Selected Topics',
+  INSA444: 'Oopen Source Netowrk Systems',
+  IPRG473: 'Multimedia System Development',
+  INET351: 'Communiation Networks',
+  INSA485: 'Internet of Things',
+};
+
+function messageBuilder(serviceRequested, basicInfo, additionalInfo, academicRecords, subjects) {
+  return new Promise(resolve => {
+    let messageText;
+    if (!basicInfo.intro) {
+      messageText =
+        'Welcome to College of Telecom and Elctornics Whatsapp trainees service platform.\n'
+      if (basicInfo.user_type === 'trainee') {
+        messageText = messageText.concat(
+          'We offer the following services:\n *Add subjects* \n *Remove subjects* \n *Contact staff* \n *Request letter* \n',
+          'To use any of the services, kindly reply with one of the text listed above and if you would like to see the list just say hi to us anytime.\n\n'
+        );
+      } else if (basicInfo.user_type === 'staff') {
+        messageText = messageText.concat(
+          "This service can be used recieve trainees' requests and help in resolving these requests \n" +
+            'To check if there is any pending requests for you, kindly send: \n *List pending*' +
+            'To check and resolve requests, kindly send: \n *#[request number]* e.g #3 or #340 \n' +
+            'By default, You are currently unregsitered in our system. To be able to receive request notification automatically, please send: \n *register*\n\n'
+        );
+      }
+      messageText =
+        messageText +
+        'You are conacting us using this phone number ' +
+        basicInfo.phone_number +
+        ' which registered to ' +
+        additionalInfo.ENfirst_name +
+        ' ' +
+        additionalInfo.ENlast_name +
+        ' on the system' +
+        '\nIf this is not you, please conact the administrator via Whatapp at +966541199915 to rectify the issue and register your number.';
+      resolve(messageText);
+    } else if (typeof serviceRequested === 'object') {
+      // this is if the add/remove request is correct and logged
+      if (serviceRequested.recorded === true) {
+        messageText =
+          'Your request ID: *' +
+          serviceRequested.requestID +
+          '* has been recorded in our system.\n' +
+          'Kindly allow for some time to pass for approval.\n' +
+          'You can check on the status of your request by typing (#) followed by the request ID.\n' +
+          'Thank you for using our services';
+      } else {
+        messageText =
+          'Apolgies, it seems your request was not recorded due to an error.\n' +
+          'Kindly send your request again.\n' +
+          'Apoliges for any inconvience caused.';
+      }
+      resolve(messageText);
+    } else if (['add', 'remove'].includes(serviceRequested)) {
+      messageText =
+        'Through this service platform, we will record your request to add/remove subjects and your requests will be sent for approval from the relevant parties.\n' +
+        'Once approved and the requested changes are implemented, you will receive a notification via the platform and your timetable should reflect the changes.\n';
+      let academicFilterChar;
+      let filteredAcademicRecords = [];
+      if (serviceRequested === 'add') {
+        // filter query result to subjects not finished
+        academicFilterChar = 'N';
+      } else {
+        // filter query result to subjects currently studying
+        academicFilterChar = 'C';
+      }
+      for (let i = 1; i < subjects.length; i++) {
+        if (academicRecords[subjects[i]] === academicFilterChar) {
+          filteredAcademicRecords.push(subjects[i]);
+        }
+      }
+      // start testing removal of alternate subejcts
+      if (!filteredAcademicRecords.includes('IPRG335' && 'INSA481 ')) {
+        filteredAcademicRecords = filteredAcademicRecords.filter(x => !['IPRG335', 'INSA481'].includes(x));
+      }
+      if (!filteredAcademicRecords.includes('INSA444' && 'IPRG473')) {
+        filteredAcademicRecords = filteredAcademicRecords.filter(x => !['INSA444', 'IPRG473'].includes(x));
+      }
+      if (!filteredAcademicRecords.includes('INET351' && 'INSA485')) {
+        filteredAcademicRecords = filteredAcademicRecords.filter(x => !['INET351', 'INSA485'].includes(x));
+      }
+      // end testing removal of alternate subejcts
+      // #todo add subjects names to the array after the subjects codes
+      if (filteredAcademicRecords.length > 0) {
+        messageText = messageText.concat('You have the following subjects you can ', serviceRequested, ':\n');
+        for (let i = 0; i < filteredAcademicRecords.length; i++) {
+          messageText = messageText.concat(
+            '*' + filteredAcademicRecords[i] + '*: '+ diction[filteredAcademicRecords[i]] +'\n'
+          );
+        }
+        messageText = messageText.concat(
+          'To request the service reply with ' +
+            serviceRequested +
+            ' followed by the subjects code.\n For example: ' +
+            serviceRequested +
+            ' MATH301'
+        );
+      } else {
+        messageText = messageText.concat('*No subject can ', serviceRequested, 'ed.*\n');
+      }
+      resolve(messageText);
+    } else if (serviceRequested === 'contact') {
+      messageText = 'get staff contact info message';
+      resolve(messageText);
+    } else if (serviceRequested === 'letter') {
+      messageText =
+        'What type of letter would you like to reques?\n' +
+        'Please reply with one of the following:\n' +
+        '*Letter of Indication*\n' +
+        '*Exams Letter*';
+      resolve(messageText);
+    } else {
+      if (serviceRequested === 'greeting') {
+        messageText =
+          'Hello, ' +
+          additionalInfo.ENfirst_name +
+          ' ' +
+          additionalInfo.ENlast_name +
+          '\nThis is the College of Telecom and Elctornics Whatsapp trainees service platform. ';
+      } else if (serviceRequested === 'unknown') {
+        messageText = 'Sorry, I could not understand what you have typed.\n';
+      }
+      messageText = messageText.concat(
+        'We offer the following services:\n *Add subjects* \n *Remove subjects* \n *Contact staff* \n *Request letter* \n' +
+          'To use any of the services, kindly reply with one of the text listed above'
+      );
+      resolve(messageText);
+    }
+    return messageText;
+  });
+}
+
+module.exports = messageBuilder;
---
+++ resources/textAnalysis.js
@@ -0,0 +1,156 @@
+var greetingsKeywords = [
+  'greeting',
+  'hello',
+  'hi',
+  'hey',
+  'good',
+  'morning',
+  'afternoon',
+  'evening',
+  'مرحبا',
+  'هلا',
+  'سلام',
+  'السلام',
+];
+var addingkeywords = [
+  'add',
+  'adding',
+  'addition',
+  'اضف',
+  'أضف',
+  'ضيف',
+  'اضافة',
+  'إضافة',
+  'اضيف',
+  'ضف',
+];
+var removeKeywords = [
+  'remove',
+  'removing',
+  'removal',
+  'drop',
+  'احذف',
+  'حذف',
+  'إزالة',
+];
+var contactKeywords = [
+  'contact',
+  'contacts',
+  'email',
+  'speak',
+  'talk',
+  'someone',
+  'staff',
+  'trainter',
+  'issue',
+  'problem',
+  'room',
+  'office',
+  'inquiry',
+  'inquire',
+  'query',
+  'ايميل',
+  'مكتب',
+  'غرفة',
+  'اتواصل',
+  'اتصال',
+  'تواصل',
+  'اتواصل',
+  'اوصل',
+  'استفسر',
+  'استفسار',
+  'المهندس',
+  'موظف',
+  'المدرب',
+  'شخص',
+  'ساعات',
+  'مكتبية',
+];
+var letterRequestKeywords = [
+  'letter',
+  'print',
+  'خطاب',
+  'تعريف',
+  'إفادة',
+  'افادة',
+];
+var listKeywords = ['list'];
+var wordLists = [
+  greetingsKeywords,
+  addingkeywords,
+  removeKeywords,
+  contactKeywords,
+  letterRequestKeywords,
+  listKeywords,
+];
+
+function textAnalysis(text, senderNumber, userType, subjects, staffList) {
+  var result = 'unknown';
+  let textKeywords;
+  textKeywords = text.toLowerCase().trim();
+  textKeywords = textKeywords.split(' ');
+  if (text.match('#')) {
+    let requestNumber = textKeywords.filter(x => {
+      return x.startsWith('#');
+    });
+    requestNumber = requestNumber.toString();
+    requestNumber = parseInt(requestNumber.replace(/[^0-9]/g,''));
+    if (requestNumber > 0) {
+      if (userType === 'staff') {
+        if (textKeywords.includes('approve')) {
+          result = {
+            serviceRequested: 'approve',
+            itemRequested: requestNumber,
+          };
+        } else if (textKeywords.includes('reject')) {
+          result = {
+            serviceRequested: 'reject',
+            itemRequested: requestNumber,
+          };
+        }
+      } else {
+        result = {
+          serviceRequested: 'requestInfo',
+          itemRequested: requestNumber,
+        };
+      }
+    }
+  } else {
+    for (let groupedKeywords of wordLists) {
+      let testingTextArray = textKeywords.filter((x) =>
+        groupedKeywords.includes(x)
+      );
+      if (testingTextArray.length > 0) {
+        let text2ndKeywords = textKeywords;
+        textKeywords = testingTextArray;
+        result = groupedKeywords[0];
+        if (result === 'add' || result === 'remove') {
+          let _2ndTestingTextArray = text2ndKeywords.filter(x => {
+            x = x.toUpperCase();
+            return subjects.includes(x);
+          });
+          if (_2ndTestingTextArray.length > 0) {
+            result = {
+              serviceRequested: result,
+              itemRequested: _2ndTestingTextArray[0],
+              recorded: false,
+            };
+          }
+        }
+        console.log('You were trying to ' + result.toString() + ' by using these keywords: ' + textKeywords);
+        break;
+      }
+    }
+  }
+  let fs = require('fs');
+  let dt = new Date();
+  let timestamp = dt.toLocaleDateString() + ' ' + dt.toLocaleTimeString();
+  fs.appendFile('./logs/textAnalysisLogs.txt', 'On ' + timestamp + ', The conclusion: ' + result + ', From: :' + senderNumber + ' Based on the following keywords: ' + textKeywords.toString() + '\n From this text: ' + text + '\n', function (err) {
+      if (err) return console.log(err);
+      console.log('Logging: Text Analysis logged.');
+    }
+  );
+  return result;
+}
+
+module.exports = textAnalysis;
