6749369d3a2f0fce7cb869a9a58584b5c807abcb
Osiam <53178416+OsamaSiam@users.noreply.github.com>
2022-05-31 23:45:26 +0300

    v0.102+++ moved array to external + added rejectReason

--- resources/subjectsData.js
+++ resources/subjectsData.js
@@ -44,4 +44,4 @@ let subjectPrerequiste = {
     INSA492: ['INSA371', 'INSA452', 'INSA454', 'INSA483']
   }
 
-module.exports.data = {diction, subjectPrerequiste};
+module.exports = {diction, subjectPrerequiste};
