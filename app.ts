5318452d91b0540aca269ad6443b215e477d28e7
Osiam <53178416+OsamaSiam@users.noreply.github.com>
2022-06-01 03:53:35 +0300

    v0.10331Fix6 updating timestamp for # reply

--- app.ts
+++ app.ts
@@ -188,8 +188,8 @@ function start(client) {
           serviceRequested.requestID = rows.insertId;
         } else if (rows !== undefined && message.body.match('#')) {
           requestsData = rows[0];
-          requestsData.request_timestamp = requestsData.request_timestamp.toString().split('\\.', 5)[0];
-          requestsData.update_timestamp = requestsData.update_timestamp.toString().split('\\.', 5)[0];
+          requestsData.request_timestamp = requestsData.request_timestamp.toString().split('\\.', 5)[1];
+          requestsData.update_timestamp = requestsData.update_timestamp.toString().split('\\.', 5)[1];
         } else if (rows !== undefined && serviceRequested.serviceRequested === 'list') {
           requestsData = rows;
           console.log('testing here app L164 raw data from sql:', rows);
